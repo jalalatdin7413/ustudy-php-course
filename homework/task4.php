e65938bdcfe16108db3c8ec8ecd2900755e3f505
jalalatdin7413 <ramanovjalalatdin@gmail.com>
2024-10-30 18:46:53 +0500

    esap islendi

---
+++ homework/task4.php
@@ -0,0 +1,45 @@
+<?php 
+  // 1-esap
+  $age = 80;
+
+  if ($age < 18) {
+    echo "Ruqsat joq";
+  } else if ($age <= 70) {
+    echo "Ruqsat";
+  } else {
+    echo "Old person";
+  }
+
+    // 2-esap
+
+    echo "<br>";
+    echo "<br>";
+
+  $ball = 45;
+
+  if ($ball < 50) {
+    echo "Jig'ildi";
+  } else if ($ball <= 69) {
+    echo "Qaniqarli";
+  } else if ($ball <= 88) {
+    echo "Jaqsi";
+  } else {
+    echo "Ayriqsha";
+  }
+  echo "<br>";
+  echo "<br>";
+
+  // 3-esap
+
+  $number = 44;
+   if ($number%2== 0) {
+    echo "Jup san";
+   } else {
+    echo "Taq san";
+   }
+
+   echo "<br>";
+   echo "<br>";
+
+   // 4-esap
+   

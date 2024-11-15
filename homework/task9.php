586bf0c9f59e504b8f1e99038044afc52437cb83
jalalatdin7413 <ramanovjalalatdin@gmail.com>
2024-11-15 18:52:43 +0500

    hmmm

--- homework/task9.php
+++ homework/task9.php
@@ -27,3 +27,86 @@ fclose($file);
 }
 
 echo "<br><br>";
+
+
+
+$file = fopen("files/file.txt", "w");
+fwrite($file, "Real Madrid.");
+fclose($file);
+
+$file = "file.txt";
+$handle = fopen($file, "a");
+$new_line = "The best club. \n";
+
+fwrite($handle, $new_line);
+
+fclose($handle);
+
+echo "Taza qatar qosildi!";
+
+echo "<br><br>";
+
+//3-esap
+
+$file = "file.txt";
+
+$handle = fopen($file, "r");
+
+$line_count = 0;
+
+while(!feof($handle)) {
+    $line = fgets($handle);
+    if ($line !== false) {
+        $line_count++;
+    }
+}
+fclose($handle);
+
+echo "Fayldagi satrlar sani: " . $line_count++;
+
+echo "<br><br>";
+// 4-esap
+
+$filename = "files/format.html";
+$htmlcontent = "<h1>Title</h1><p>This is a paragtaph with <strong>bold</strong:> text.</p>";
+file_put_contents($filename, $htmlcontent);
+echo "HTML content written to '$htmlcontent'. ";
+
+echo "<br>";
+
+$filesize = filesize($filename);
+
+echo "Fayl olshemi: $filesize bayt.";
+
+echo "<br><br>";
+
+// 5-esap
+
+$dirname = "new_car";
+
+if(!is_dir($dirname)) {
+    mkdir($dirname); 
+    echo "Directory '$dirname' bar.";
+} else {
+    echo "directory '$dirname' joq";
+}
+
+$fileName = $dirname . "/my_file.txt";
+
+$fileHandle = fopen($fileName, "w");
+
+$textWrite = [
+    "First qatar. \n",
+    "second qatar. \n",
+];
+fclose($fileHandle);
+
+echo "Fayl jaratildi";
+
+echo "<br><br>";
+
+// 6-esap
+
+
+
+
